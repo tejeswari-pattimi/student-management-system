12de3b4b002787832e8ad1183bc3bd172a3a577f
Tejeswari.Pattimi <tejakrishna9966@gmail.com>
2026-06-08 09:56:11 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+conn = sqlite3.connect("students.db")
+
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS students(
+    roll TEXT,
+    name TEXT,
+    marks TEXT
+)
+""")
+
+conn.commit()
